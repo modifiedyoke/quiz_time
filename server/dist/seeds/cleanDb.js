0a155625803d32699c9fdd0163d947ccbd853ed5
modifiedyoke <b.shurts@outlook.com>
2024-11-25 11:42:45 -0600

    added circleci yaml

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,17 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        if (models[modelName].db.db) {
+            let modelExists = await models[modelName].db.db.listCollections({
+                name: collectionName
+            }).toArray();
+            if (modelExists.length) {
+                await db.dropCollection(collectionName);
+            }
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+};
